ac38dc57c92f13e1d48c222cd9e5f99c4c746ec4
Michał Grabowski <mkg.grabowski@gmail.com>
2017-04-10 12:30:19 +0200

    Erased lab4/zad2, it needs to be rewritten due to debugging problems

--- lab4/zad2/src/main.c
+++ lab4/zad2/src/main.c
@@ -1,52 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <signal.h>
-#include <sys/types.h>
+#include <assert.h>
 
-typedef int bool;
-#define true 1
-#define false 0
+int miracleHappenned = 0;
 
-static int direction = 1;
-static const char *SIGNAL_ERROR = "Error occured while setting a signal handler";
-static const char *SIGNAL_MESSAGE = "Caught SIGTSTP, reversing";
-static const char *EXIT_MESSAGE = "Received SIGINT, quitting";
-
-void signalHandler(int signr);
-
+// THIS WILL BE FINISHED SOMEDAY
 int main(int argc, char *argv[]){
-	struct sigaction action;
-	action.sa_handler = signalHandler;
-	sigemptyset(&action.sa_mask);
-	action.sa_flags = 0;
-	if(sigaction(SIGINT, &action, NULL) == -1){
-		fprintf(stderr, "%s\n", SIGNAL_ERROR);
-		exit(EXIT_FAILURE);
-	}
-	if(signal(SIGTSTP, signalHandler) == SIG_ERR){
-		fprintf(stderr, "%s\n", SIGNAL_ERROR);
-		exit(EXIT_FAILURE);
-	}
-	pid_t pid = getpid();
-	printf("My pid: %d\n", pid);
-
-	for(int i = 0;; i = (i + 26 + direction) % 26){
-		printf("%c\n", (char) 65 + i);
-		sleep(1);
-	}
-
+	assert(miracleHappenned);
 	return 0;
-}
-
-void signalHandler(int signr){
-	if(signr == SIGTSTP){
-	printf("\n%s\n", SIGNAL_MESSAGE);
-		direction *= -1;
-	}
-	if(signr == SIGINT){
-		printf("\n%s\n", EXIT_MESSAGE);
-		exit(0);
-	}	
 }
